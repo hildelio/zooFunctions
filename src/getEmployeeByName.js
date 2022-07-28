a44781570ba71dcbecfb2d16636b0cdea770d7b9
hildelio <hildelio@gmail.com>
2022-07-28 16:33:11 -0300

    Requisito 3 cumprido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  const filtered = (n) => n.firstName === employeeName || n.lastName === employeeName;
+  const found = employees.find(filtered);
+  return found === undefined ? {} : found;
 };
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
