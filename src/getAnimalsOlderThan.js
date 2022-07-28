685ee7906707b805f44bbace8e415b17034d696f
hildelio <hildelio@gmail.com>
2022-07-28 15:54:28 -0300

    Requisito 2 cumprido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) => species
+  .filter((a) => a.name === animal)[0].residents.every((e) => e.age >= age);
 
 module.exports = getAnimalsOlderThan;
