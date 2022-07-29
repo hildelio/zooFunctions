9e7e625c36f443fada4d60f8391b2bd1b4a7e15a
hildelio <hildelio@gmail.com>
2022-07-29 14:15:51 -0300

    Requisito 5 cumprido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = ({ specie = false, sex = false } = {}) => {
+  if (!specie && !sex) {
+    const allAnimals = {};
+    species.forEach((e) => { allAnimals[e.name] = e.residents.length; });
+    return allAnimals;
+  }
+  const theAnimal = species.find((e) => e.name === specie).residents;
+  if (!sex) {
+    return theAnimal.length;
+  }
+  const animalSexn = theAnimal.filter((e) => e.sex === sex).length;
+  return animalSexn;
+};
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,5 @@
-const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
   const filtered = (a) => species.filter((element) => element.id === a)[0];
