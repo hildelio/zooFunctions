83929b92b3ecce5c1c2c3ca640f3097ee72599b5
hildelio <hildelio@gmail.com>
2022-08-01 12:12:42 -0300

    Requisito 10 concluido.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const getOldestFromFirstSpecies = (id) => {
+  const person = employees.find((c) => c.id === id);
+  const animalId = person.responsibleFor[0];
+  const specie = species.find((s) => s.id === animalId);
+  const oldest = specie.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(oldest);
+};
 
 module.exports = getOldestFromFirstSpecies;
