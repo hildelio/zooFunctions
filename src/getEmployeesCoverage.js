3d6c8bb0bfcb63ea3e5323479ce8fe6e338b599c
hildelio <hildelio@gmail.com>
2022-08-02 17:31:43 -0300

    Requisito 11 concluido.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,51 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const allEmployees = () => {
+  const people = [];
+  employees.forEach((employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    const animals = [];
+    const region = [];
+    responsibleFor.forEach((animalId) => {
+      const theAnimal = species.find((a) => a.id === animalId);
+      animals.push(theAnimal.name);
+      region.push(theAnimal.location);
+    });
+    const obj = { id, fullName: `${firstName} ${lastName}`, species: animals, locations: region };
+    people.push(obj);
+  });
+  return people;
+};
+const idByName = (name) => {
+  if (employees.some((e) => e.firstName === name || e.lastName === name)) {
+    const all = allEmployees();
+    const person = employees.find((emp) => emp.firstName === name || emp.lastName === name);
+    console.log(person);
+    const idFounded = person.id;
+    return all.find((e) => e.id === idFounded);
+  }
+  throw new Error('Informações inválidas');
+};
+
+const byId = (id) => {
+  if (employees.some((e) => e.id === id)) {
+    return allEmployees().find((p) => p.id === id);
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployeesCoverage(param = {}) {
+  const { name = false, id = false } = param;
+  if (!name && !id) {
+    return allEmployees();
+  }
+  if (name) {
+    return idByName(name);
+  }
+  if (id) {
+    return byId(id);
+  }
 }
 
 module.exports = getEmployeesCoverage;
